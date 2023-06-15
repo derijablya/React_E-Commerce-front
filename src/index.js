3ce1d620c8c9dbfcf5ffe1d1e9820b0ae580f597
derijablya <dulugov@gmail.com>
2023-06-15 14:00:51 +0300

    fix fix fix

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ root.render(
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/product" element={<RequireToken><Products /></RequireToken>} />
+        <Route path="/product" element={<Products />} />
         <Route path="/product/:id" element={<Product />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
